70319da159c591531140987a9c5616e9cf55923e
Jose Rojas <jalienr17@icloud.com>
2026-05-11 14:06:55 -0400

    Added comments to the checkout.js file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,5 @@
 import {
+  // Imports necessary files to run this page as modules
   generateCartHtml,
   deliveryOptionsHtml,
 } from "./checkout/order-summary.js";
@@ -12,7 +13,10 @@ import { loadPageAPI } from "../backend/backend-practice.js";
 });*/
 
 loadPageAPI(() => {
-  deliveryOptionsHtml();
-  generatePaymentSummary();
-  generateCartHtml();
+  // Runs the load page function defined in the backend file. This is the most up to date
+  // way to load from an API as this function uses the async await to load the cart and products promise then
+  // runs the functions that use this data to render the page. Very clean and simple syntax.
+  deliveryOptionsHtml(); // shows the options html and all of its content.
+  generatePaymentSummary(); // shows the payment summary and its calculations.
+  generateCartHtml(); // shows the carts html with the relevant products.
 });
