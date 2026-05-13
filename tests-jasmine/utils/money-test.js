eef334de0bd8b13502b2cfa245a20f1ff6a78fdb
Jose Rojas <jalienr17@icloud.com>
2026-05-13 13:51:10 -0400

    Addes comments to the money-test.js file within Jasmine's framework.

--- tests-jasmine/utils/money-test.js
+++ tests-jasmine/utils/money-test.js
@@ -1,13 +1,28 @@
-import { calcTax, formatCurrency } from "../../scripts/utils/money.js";
+import { calcTax, formatCurrency } from "../../scripts/utils/money.js"; // Importing calcTax() and
+// formatCurrency() in order to test with the Jasmine framework. Note: this file is linked through the script
+// tag and the module attribute in the tests.html file within Jasmine just how a normal script link would work
+//  just kept under Jasmine's own script tags to properly load the framework.
 
 describe("Test Suite: calcTax", () => {
+  // Describe is the Jasmine function that is used to group related
+  // tests together. The first parameter takes a string that displays in the UI as a title. While the second
+  // parameter takes a function in which the test functions will be used.
   describe("Nesting describe is possible for grouping related tests like so :)", () => {
-    it("Working test is a test thats just checks if the function is working at its most basic operation, in this case = calculates tax at 10% and converts to dollares from cents", () => {
-      expect(calcTax(2010)).toEqual("2.01");
-    });
+    // It's also possible
+    // to nest describe functions. For more specific grouping.
+    it("Working test is a test thats just checks if the function is working at its most basic operation, in this case = calculates tax at 10% and converts to dollars from cents", () => {
+      expect(calcTax(2010)).toEqual("2.01"); // it(); also takes 2 parameters. The first a string to further
+      //  describe that specific test and display it as a sub-title on the page. The second is a function that
+      //  holds the test and uses certain functions to perform the test. In this one the expect function with
+      // the toEqual(); method is used. Jasmine is made to be read like reading english sentences. expect();
+      // takes the function being tested as a parameter while chaining the toEqual(); method to check the
+      // results.
+    }); // In Jasmine instead of just seeing "Failed" in a console, Jasmine provides a UI that tells you
+    // exactly which edge case failed and why.
 
     it("Edge case test is a test that checks if the function is working at its limits, where it would be expected to fail, in this case, at the .055 it should round up", () => {
-      expect(calcTax(2015)).toEqual("2.02");
+      expect(calcTax(2015)).toEqual("2.02"); // The same pattern is repeated while using working tests and
+      // following with edge case tests.
     });
 
     it("Rounds Down", () => {
